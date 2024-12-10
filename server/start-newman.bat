62bb1e2f15dc8265a80ebed7805b6e729ee11021
Thomas Seitz <thomas.seitz@telefonica.com>
2024-12-10 14:30:15 +0100

    version numbers should be 1.0.1 Fixes #37

--- server/start-newman.bat
+++ server/start-newman.bat
@@ -1,1 +1,1 @@
-newman run -d ..\testing\dataFile.json ../testing/NetworkDataLakeProxyBasicTest_1.0.0.postman_collection.json --env-var baseUrl=http://localhost:4019 --delay-request 1000
+newman run -d ..\testing\dataFile.json ../testing/NetworkDataLakeProxyBasicTest_1.0.1.postman_collection.json --env-var baseUrl=http://localhost:4019 --delay-request 1000
